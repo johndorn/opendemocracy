b8e2d3c1f46acccdc894c5568102f9bd8986f244
johndorn <john.dorn@gmail.com>
2012-11-09 23:58:05 -0500

    started building auth

--- app.js
+++ app.js
@@ -6,23 +6,31 @@
 var express = require('express') // express already extends EventEmitter
   , path = require('path')
   , mongoose = require('mongoose')
-  , events = require('events');
+  , events = require('events')
+  , routes = require('./routes');
 
-// mongoose.connect('mongo://localhost/mir');
+var db = mongoose.connect('mongo://localhost/opendemocracy');
 
 /**
  */
 
-
 var app = express();
 
 app.configure(function(){
-	//app.set('port', process.env.PORT || 3001);
-	app.set('port', 3001);
+	app.set('port', 80);
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+	app.use(express.session({
+		store: {
+			dbname: db.db.databaseName,
+			host: db.db.serverConfig.host,
+			port: db.db.serverConfig.port,
+			username: db.uri.username,
+			password: db.uri.password
+		}
+	}));
 	app.use(app.router);
 	app.use(express.static(path.join(__dirname, 'public')));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 
 exports.index = function(req, res) {
-
+	var session = req.session;
+	
 };
